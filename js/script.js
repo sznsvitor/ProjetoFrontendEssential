9ddf1a6afa1fc180d1e5dccfd38ff7b63457130c
th1agOx <thdarochabp@gmail.com>
2026-05-05 20:56:42 -0300

    consumindo API da openWeather e criando form

--- js/script.js
+++ js/script.js
@@ -1,22 +1,52 @@
-function buscarClima() {
-    const cidade = document.getElementById("cidade").value.trim();
-    const resultado = document.getElementById("resultado");
+document.addEventListener('DOMContentLoaded', () => {
+    const form = document.getElementById('form')
 
-    if (cidade === "") {
-        alert("Digite uma cidade!");
-        return;
+    form.addEventListener('submit', async (event) => {
+        event.preventDefault();
+
+        const cidade = document.getElementById('cidade').value.trim();
+
+        if (!cidade) {
+            return console.erro("erro no id")
+        }
+
+        await obterClima(cidade);
+    });
+});
+
+async function obterClima(cidade) {
+    try {
+        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cidade}&appid=85475d1b877042e5c24efc58c7f69373&units=metric&lang=pt_br`)
+
+        if (!response.ok)
+            throw new Error('Cidade não encontrada');
+
+        const data = await response.json()
+        exibirDados(data)
+    } catch (erro) {
+        alert(erro.message)
     }
+}
 
-    const temperatura = Math.floor(Math.random() * 30) + 10;
-
-    const climas = ["☀️ Ensolarado", "🌧️ Chuvoso", "⛅ Nublado", "🌩️ Tempestade"];
-    const clima = climas[Math.floor(Math.random() * climas.length)];
+function exibirDados(data) {
+    const resultado = document.getElementById('resultado')
 
     resultado.innerHTML = `
-        <div class="card p-4 shadow">
-            <h3>${cidade}</h3>
-            <p>${clima}</p>
-            <h4>${temperatura}°C</h4>
+        <div class="info-temperatura" style="max-width: 400px">
+            <div class="container">
+                <h3 class="cidade">${data.name}</h3>
+                <p class="temperatura" style="font-size: 1.6rem" > Temperatura: ${data.main.temp} °C</p>
+                <p class="umidade"> Max:${data.main.temp_max} Min:${data.main.temp_min}</p>
+                <p class="umidade"> Umidade: ${data.main.humidity} %</p>
+                <p class="clima"> Clima: ${data.weather[0].description}</p>
+            </div>
         </div>
-    `;
+    `
 }
+
+//Interação do menu
+
+document.getElementById('menu-icon').addEventListener('click', function () {
+    const menu = document.getElementById('menu-option');
+    menu.classList.toggle('show');
+})
